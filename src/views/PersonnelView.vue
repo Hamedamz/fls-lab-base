c22af2ff2ba5fd6906d0ee95ad1a781963426459
Nima <Niyazdan@ucsd.edu>
2023-11-01 08:58:04 -0700

    Edited Personnel Page

--- src/views/PersonnelView.vue
+++ src/views/PersonnelView.vue
@@ -46,7 +46,7 @@ const personnelGroups = ref([
     title: "Lab Director",
     items: [
       {
-        name: "Professor Shahram Ghandeharizadeh",
+        name: "Shahram Ghandeharizadeh",
         email: "shahram at usc dot edu",
         photo: ShahramGhandeharizadeh,
         bio: "Shahram Ghandeharizadeh received his Ph.D. degree in Computer Science from the University of Wisconsin, Madison, in 1990. Since then, he has been on the faculty at the University of Southern California. In 1992, Dr. Ghandeharizadeh received the National Science Foundation Young Investigator's Award for his research on the physical design of parallel database systems. In 1995, he received an award from the School of Engineering at USC in recognition of his research activities. He was a recipient of the ACM Software System Award 2008 for his contributions to Gamma, a scalable database management system. His research interests include design, implementation and evaluation of novel architectures for high performance data intensive applications, multimedia-based social networking systems, parallel database systems, and active databases. He has served on the organizing committees of numerous conferences; most recently as the general chair of FAB 2019 and VLDB 2019. His activities are supported by several grants from the National Science Foundation, Oracle, Microsoft, BMC Software, and Hewlett-Packard. He is the director of the database laboratory at USC. He was a member of ACM SIGMOD executive committee and the Editor-in-Chief of ACM SIGMOD DiSC from 2003 to 2006. He served as a member of the Council of Directors of Kodaikanal International School from 2005 to 2008.",
@@ -58,7 +58,7 @@ const personnelGroups = ref([
     title: "Affiliated Faculty",
     items: [
       {
-        name: "Professor Heather Culbertson",
+        name: "Heather Culbertson",
         email: "hculbert at usc dot edu",
         photo: HeatherCulbertson,
         bio: "Heather Culbertson is a Gabilan Assistant Professor of Computer Science at the University of Southern California. Her research focuses on the design and control of haptic devices and rendering systems, human-robot interaction, and virtual reality. Particularly she is interested in creating haptic interactions that are natural and realistically mimic the touch sensations experienced during interactions with the physical world. Previously, she was a research scientist in the Department of Mechanical Engineering at Stanford University where she worked in the Collaborative Haptics and Robotics in Medicine (CHARM) Lab. She received her PhD in the Department of Mechanical Engineering and Applied Mechanics (MEAM) at the University of Pennsylvania in 2015 working in the Haptics Group, part of the General Robotics, Automation, Sensing and Perception (GRASP) Laboratory. She completed a Masters in MEAM at the University of Pennsylvania in 2013, and earned a BS degree in mechanical engineering at the University of Nevada, Reno in 2010. She is currently serving as Publications Chair for IEEE Haptics Symposium. Her awards include the NSF CAREER Award, IEEE Technical Committee on Haptics Early Career Award, citation for meritorious service as a reviewer for the IEEE Transactions on Haptics, Best Paper at UIST 2017, and the Best Hands-On Demonstration Award at IEEE World Haptics 2013.",
