c6d9bab4d48cb66a71287ee5c86fd68d87abdd7f
Nima <Niyazdan@ucsd.edu>
2023-11-25 16:34:19 -0800

    Holodeck publications update

--- src/views/PublicationsView.vue
+++ src/views/PublicationsView.vue
@@ -8,14 +8,18 @@
           <div>
             <div class="flex items-baseline">
               <span class="citation-number text-white mr-2">{{ i + 1 }}.</span>
-              <p class="citation-text">{{ pub.cite }}</p>
+              <p class="citation-text">
+                {{ pub.citeBeforeLink }}
+                <a :href="pub.conferenceLink" target="_blank" class="conference-link">
+                  {{ pub.conferenceTitle }}
+                </a>
+                {{ pub.location }}{{ pub.citeAfterLink }}
+              </p>
             </div>
           </div>
           <div class="mt-4 flex justify-end">
             <a v-if="pub.pdf" :href="pub.pdf" target="_blank" class="pdf-link mr-2">pdf</a>
-            <a v-if="i === 0" href="https://github.com/flyinglightspeck/Group-Formation" target="_blank" class="source-code-link">Source Code</a>
-            <a v-if="i === 4" href="https://github.com/flyinglightspeck/FLSbagfile" target="_blank" class="source-code-link">Source Code</a>
-            <a v-if="i === 5" href="https://github.com/flyinglightspeck/Motill" target="_blank" class="source-code-link">Source Code</a>
+            <!-- Include source code links if any -->
           </div>
         </div>
       </div>
@@ -33,40 +37,81 @@
 </template>
 
 <script setup>
-import NSFLogoImage from "@/assets/images/NSF_logo.png"; // Importing the NSF logo image
+import NSFLogoImage from "@/assets/images/NSF_logo.png";
 const publications = [
+{
+  citeBeforeLink: 'H. Alimohammadzadeh, R. Bernard, Y. Chen, T. Phan, P. Singh, S. Zhu, H. Culbertson, and S. Ghandeharizadeh. Dronevision: An Experimental 3D Testbed for Flying Light Specks. In ',
+  conferenceLink: 'http://www.holodecks.quest',
+  conferenceTitle: 'the First International Conference on Holodecks',
+  location: 'Los Angeles, USA',
+  citeAfterLink: ', December 15, 2023. (arXiv 2308.10121, August 2023.)',
+  pdf: 'https://arxiv.org/pdf/2308.10121.pdf',
+},
   {
-    cite: 'H. Alimohammadzadeh, H. Culbertson, and S. Ghandeharizadeh. An Evaluation of Decentralized Group Formation Techniques for Flying Light Specks. In ACM Multimedia Asia, Taipei, Taiwan, December 6-8, 2023.',
+    citeBeforeLink: 'H. Alimohammadzadeh, H. Culbertson, and S. Ghandeharizadeh. An Evaluation of Decentralized Group Formation Techniques for Flying Light Specks. In ACM Multimedia Asia, Taipei, Taiwan, December 6-8, 2023.',
     pdf: '',
   },
   {
-    cite: 'Y. Chen, H. Alimohammadzadeh, S. Ghandeharizadeh, H. Culbertson. Towards Enabling Complex Touch-based Human-Drone Interaction. In Workshop on Human Multi-Robot Interaction, IROS, October 1, 2023, Detroit, USA.',
+    citeBeforeLink: 'Y. Chen, H. Alimohammadzadeh, S. Ghandeharizadeh, H. Culbertson. Towards Enabling Complex Touch-based Human-Drone Interaction. In Workshop on Human Multi-Robot Interaction, IROS, October 1, 2023, Detroit, USA.',
     pdf: 'https://bpb-us-e2.wpmucdn.com/sites.uci.edu/dist/2/5230/files/2023/09/79_SCR_23_Yang_Chen.pdf',
   },
   {
-    cite: "T. Phan, H. Alimohammadzadeh, H. Culbertson, and S. Ghandeharizadeh. An Evaluation of Three Distance Measurement Technologies for Flying Light Specks. In International Conference on Intelligent Metaverse Technologies and Applications (iMETA2023), Tartu, Estonia, September 18-20, 2023.",
+    citeBeforeLink: "T. Phan, H. Alimohammadzadeh, H. Culbertson, and S. Ghandeharizadeh. An Evaluation of Three Distance Measurement Technologies for Flying Light Specks. In International Conference on Intelligent Metaverse Technologies and Applications (iMETA2023), Tartu, Estonia, September 18-20, 2023.",
     pdf: "https://arxiv.org/pdf/2308.10115.pdf",
   },
   {
-    cite: "H. Alimohammadzadeh, R. Bernard, Y. Chen, T. Phan, P. Singh, S. Zhu, H. Culbertson, and S. Ghandeharizadeh. Dronevision: An Experimental 3D Testbed for Flying Light Specks. In arXiv 2308.10121, August 2023.",
-    pdf: "https://arxiv.org/pdf/2308.10121.pdf",
-  },
-  {
-    cite: "H. Alimohammadzadeh, D. Mehraban, and S. Ghandeharizadeh. Modeling Illumination Data with Flying Light Specks. In Proceedings of the 14th Conference on ACM Multimedia Systems, Vancouver, Canada, June 2023.",
+    citeBeforeLink: "H. Alimohammadzadeh, D. Mehraban, and S. Ghandeharizadeh. Modeling Illumination Data with Flying Light Specks. In Proceedings of the 14th Conference on ACM Multimedia Systems, Vancouver, Canada, June 2023.",
     pdf: "https://dl.acm.org/doi/pdf/10.1145/3587819.3592544",
   },
   {
-    cite: "S. Ghandeharizadeh. Display of 3D Illuminations using Flying Light Specks. In ACM Multimedia, Lisbon, Portugal, October 2022.",
+    citeBeforeLink: "S. Ghandeharizadeh. Display of 3D Illuminations using Flying Light Specks. In ACM Multimedia, Lisbon, Portugal, October 2022.",
     pdf: "https://arxiv.org/pdf/2207.08346.pdf",
   },
   {
-    cite: "S. Ghandeharizadeh and L. Garcia. Safety in the Emerging Holodeck Applications. In CHI 2022 Workshop on Novel Challenges of Safety, Security and Privacy in Extended Reality, April 25, 2022.",
+    citeBeforeLink: "S. Ghandeharizadeh and L. Garcia. Safety in the Emerging Holodeck Applications. In CHI 2022 Workshop on Novel Challenges of Safety, Security and Privacy in Extended Reality, April 25, 2022.",
     pdf: "https://arxiv.org/pdf/2208.08398.pdf",
   },
   {
-    cite: "S. Ghandeharizadeh. Holodeck: Immersive 3D Displays Using Swarms of Flying Light Specks. In ACM Multimedia Asia, Gold Coast, Australia, December 1-3, 2021.",
+    citeBeforeLink: "S. Ghandeharizadeh. Holodeck: Immersive 3D Displays Using Swarms of Flying Light Specks. In ACM Multimedia Asia, Gold Coast, Australia, December 1-3, 2021.",
     pdf: "https://arxiv.org/pdf/2111.03657.pdf",
   },
+  {
+    citeBeforeLink: 'S. Ghandeharizadeh and V. Oria. Virtual Reality, Augmented Reality, Mixed Reality, Holograms and Holodecks. In ',
+    conferenceLink: 'http://www.holodecks.quest',
+    conferenceTitle: 'the First International Conference on Holodecks',
+    location: 'Los Angeles, USA',
+    citeAfterLink: ', December 15, 2023.',
+    pdf: '',
+    sourceCode: ''
+  },
+  {
+    citeBeforeLink: 'H. Alimohammadzadeh and S. Ghandeharizadeh. SwarMer: A Decentralized Localization Framework for Flying Light Specks. In ',
+    conferenceLink: 'http://www.holodecks.quest',
+    conferenceTitle: 'the First International Conference on Holodecks',
+    location: 'Los Angeles, USA',
+    citeAfterLink: ', December 15, 2023.',
+    pdf: '',
+    sourceCode: ''
+  },
+{
+  citeBeforeLink: 'S. Zhu and S. Ghandeharizadeh. Flight Patterns for Swarms of Drones. In ',
+  conferenceLink: 'http://www.holodecks.quest',
+  conferenceTitle: 'the First International Conference on Holodecks',
+  location: ', Los Angeles, USA',
+  citeAfterLink: ', December 15, 2023.',
+  pdf: '',
+  sourceCode: ''
+},
+{
+  citeBeforeLink: 'N. Yazdani, H. Alimohammadzadeh, and S. Ghandeharizadeh. A Conceptual Model of Intelligent Multimedia Data Rendered using Flying Light Specks (Invited Paper). In ',
+  conferenceLink: 'http://www.holodecks.quest',
+  conferenceTitle: 'the First International Conference on Holodecks',
+  location: 'Los Angeles, USA',
+  citeAfterLink: ', December 15, 2023.',
+  pdf: '',
+  sourceCode: ''
+},
+
 ];
 </script>
 
@@ -92,17 +137,20 @@ const publications = [
 }
 
 .citation-number {
-  font-weight: bold; /* Makes the number bold */
+  font-weight: bold;
 }
 
 .citation-text {
-  color: white; /* This will make your citation text white */
-  margin: 0; /* Removes default paragraph margins */
+  color: white;
+  margin: 0;
 }
 
-.pdf-link, .source-code-link {
-  color: #63b3ed; /* Adjust the color as needed */
-  text-decoration: none; /* Removes underline from links */
-  margin-right: 10px; /* Adds spacing between links */
+.conference-link, .pdf-link, .source-code-link {
+  color: #63b3ed; /* Blue color for the links */
+  text-decoration: none; /* Removes underline */
+}
+
+.conference-link:hover, .pdf-link:hover, .source-code-link:hover {
+  color: #3182ce; /* Darker blue on hover */
 }
 </style>
